3d6d9fac94ba0695685aeb20dee865f13a2373a0
yzzeee <goooooodgood@gmail.com>
2022-03-05 21:12:20 +0900

    액티비티 사후 처리 - ActivityResultLauncher

--- app/src/main/java/com/example/cogito/MainActivity.kt
+++ app/src/main/java/com/example/cogito/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.cogito
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val result = it.data?.getStringExtra("name")
+            val mainTextView = findViewById<TextView>(R.id.main_text)
+            mainTextView.text = result
+        }
+
         val goToDetailBtn = findViewById<Button>(R.id.go_to_detail)
         goToDetailBtn.setOnClickListener{
             val intent = Intent(this, DetailActivity::class.java)
-            startActivityForResult(intent, 10)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 10 && resultCode == Activity.RESULT_OK) {
-            val result = data?.getStringExtra("name")
-            val mainTextView = findViewById<TextView>(R.id.main_text)
-            mainTextView.text = result
+            resultLauncher.launch(intent)
         }
     }
 }
